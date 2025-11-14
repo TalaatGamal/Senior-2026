922d33bf4fd515075c98f74b66e65f58157bfd21
TalaatGamal <talaatgamal.dev@gmail.com>
2025-11-14 22:59:28 +0200

    ol

--- JS/sidbar.js
+++ JS/sidbar.js
@@ -1,4 +1,5 @@
 
+const side = document.getElementById("side");
 const icon = document.getElementById("icon");
 const bars = document.getElementById("bars");
 const x_mark = document.getElementById("x-mark");
@@ -7,8 +8,12 @@ icon.addEventListener("click", () => {
     if (bars.style.display === "none") {
         bars.style.display = "block";
         x_mark.style.display = "none";
+        // side.style.right="-100vw"
+        side.style.left="-70vw"
     } else {
         bars.style.display = "none";
         x_mark.style.display = "block";
+        // side.style.right="0vw"
+        side.style.left="0vw"
     }
 });
