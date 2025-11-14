4d7fdb02c519bfafae4ba4a91b9a59e396d01d8d
TalaatGamal <talaatgamal.dev@gmail.com>
2025-11-14 23:33:59 +0200

    opi

--- JS/sidbar.js
+++ JS/sidbar.js
@@ -1,3 +1,27 @@
+// const side = document.getElementById("side");
+// const icon = document.getElementById("icon");
+// const bars = document.getElementById("bars");
+// const x_mark = document.getElementById("x-mark");
+
+// icon.addEventListener("click", () => {
+//     if (bars.style.display === "none") {
+//         bars.style.display = "block";
+//         x_mark.style.display = "none";
+//         side.style.left = "-100vw";
+//     } else {
+//         bars.style.display = "none";
+//         x_mark.style.display = "block";
+//         side.style.left = "0vw";
+//     }
+// });
+
+// side.addEventListener("click", () => {
+//     if (side.style.left === "0vw") {
+//         side.style.left = "-100vw";
+//         bars.style.display = "block";
+//         x_mark.style.display = "none";
+//     }
+// });
 
 const side = document.getElementById("side");
 const icon = document.getElementById("icon");
@@ -8,12 +32,22 @@ icon.addEventListener("click", () => {
     if (bars.style.display === "none") {
         bars.style.display = "block";
         x_mark.style.display = "none";
-        // side.style.right="-100vw"
-        side.style.left="-70vw"
+        side.style.left = "-100vw";
     } else {
         bars.style.display = "none";
         x_mark.style.display = "block";
-        // side.style.right="0vw"
-        side.style.left="0vw"
+        side.style.left = "0vw";
+    }
+});
+
+side.addEventListener("click", (e) => {
+    const sideWidth = side.offsetWidth;
+    const clickX = e.offsetX;
+    const limit = sideWidth * 0.7;
+
+    if (side.style.left === "0vw" && clickX > limit) {
+        side.style.left = "-100vw";
+        bars.style.display = "block";
+        x_mark.style.display = "none";
     }
 });
